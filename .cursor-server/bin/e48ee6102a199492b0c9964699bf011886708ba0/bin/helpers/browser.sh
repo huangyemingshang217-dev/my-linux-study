e5b7eb14ac09143ff583b142e0f3dc49f526ef24
huangyemingshang217@gmail.com <akiya@TABLET-4CI34UFI.localdomain>
2026-06-13 15:25:54 +0900

    update

---
+++ .cursor-server/bin/e48ee6102a199492b0c9964699bf011886708ba0/bin/helpers/browser.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+#
+# Copyright (c) Microsoft Corporation. All rights reserved.
+#
+ROOT="$(dirname "$(dirname "$(dirname "$(readlink -f "$0")")")")"
+
+APP_NAME="cursor"
+VERSION="3.7.27"
+COMMIT="e48ee6102a199492b0c9964699bf011886708ba0"
+EXEC_NAME="cursor"
+CLI_SCRIPT="$ROOT/out/server-cli.js"
+"$ROOT/node" "$CLI_SCRIPT" "$APP_NAME" "$VERSION" "$COMMIT" "$EXEC_NAME" "--openExternal" "$@"
---
+++ .cursor-server/data/User/History/-44c7d06e/YCYU
@@ -0,0 +1,5 @@
+alias ga='git add .'
+alias gc='git commit -m'
+alias gp='git push'
+alias gsave='git add . && git commit -m "update" && git push'
+
